85d88583bac108f5e0b6ef221739f32259bef70d
Atha Kouroussis <akouroussis@gmail.com>
2011-03-26 18:49:51 -0300

    Added submodule and user options to git::clone definition

--- manifests/definitions/clone.pp
+++ manifests/definitions/clone.pp
@@ -1,4 +1,4 @@
-define git::clone ($source, $localtree="/srv/git/", $real_name=false, $branch=false) {
+define git::clone ($source, $localtree="/srv/git/", $real_name=false, $branch=false, $submodules=false, $user="") {
 	include git::client
 	
 	if $real_name {
@@ -23,20 +23,50 @@ define git::clone ($source, $localtree="/srv/git/", $real_name=false, $branch=fa
 	# 	}
 
 	exec { "git_clone_exec_$localtree/$_name":
-		command => "/usr/bin/git clone `echo $source | sed -r -e 's,(git://|ssh://)(.*)//(.*),\\1\\2/\\3,g'` $_name",
+		command => "/usr/bin/git clone `echo ${source} | sed -r -e 's,(git://|ssh://)(.*)//(.*),\\1\\2/\\3,g'` ${_name}",
 		cwd     => $localtree,
-		creates => "$localtree/$_name/.git/",
+		creates => "${localtree}/${_name}/.git/",
+		user    => $user ? {
+			""      => undef,
+			default => $user
+		},
 		require => Class["git::client"]
 	}
 
 	case $branch {
 		false: {}
 		default: {
-			exec { "git_clone_checkout_$branch_$localtree/$_name":
-				command => "git checkout --track -b $branch origin/$branch",
-				cwd     => "$localtree/$_name",
-				creates => "$localtree/$_name/.git/refs/heads/$branch",
-				require => Exec["git_clone_exec_$localtree/$_name"]
+			exec { "git_clone_checkout_${branch}_${localtree}/${_name}":
+				command => "git checkout --track -b ${branch} origin/${branch}",
+				cwd     => "${localtree}/${_name}",
+				creates => "${localtree}/${_name}/.git/refs/heads/${branch}",
+				user    => $user ? {
+					""      => undef,
+					default => $user
+				},
+				require => Exec["git_clone_exec_${localtree}/${_name}"]
+            }
+
+			if $submodules {
+				Exec["git_update_subomodules_${localtree}/${_name}"] {
+					require +> Exec["git_clone_checkout_${branch}_${localtree}/${_name}"]
+				}
+			}
+        }
+    }
+
+	case $submodules {
+		false: {}
+		default: {
+			exec { "git_update_subomodules_${localtree}/${_name}":
+				command => "git submodule update --init --recursive",
+				cwd     => "${localtree}/${_name}",
+				onlyif  => "git submodule status | grep ^-",
+				user    => $user ? {
+					""      => undef,
+					default => $user
+				},
+				require => Exec["git_clone_exec_${localtree}/${_name}"]
             }
         }
     }
